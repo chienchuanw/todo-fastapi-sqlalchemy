516db13ed7288ceef46a6041ca3ed40563d56b81
Chuan (home) <chienchuanwww@gmail.com>
2025-05-11 22:46:54 +0800

    add: define Todo model and establish database base class

--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from app.config import DATABASE_URL
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
 
 
 async def get_db():
